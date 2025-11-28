ad82c3d196f59e8c0a89cc39bbecc3b205fc791e
Adhil Ameen <adhilameen@proton.me>
2025-11-28 11:59:32 +0530

    Fix UI bug on admin dashboard
    Changed width to 12 px for custom days input box

--- src/components/admin/Dashboard/Dashboard.jsx
+++ src/components/admin/Dashboard/Dashboard.jsx
@@ -80,7 +80,7 @@ export default function Dashboard() {
                   <button className={`${days == 30 ? 'bg-[#ffc107] text-black' : 'bg-[#000000] hover:bg-[#ffc107] hover:text-black'} py-1 px-1.5 rounded-md font-medium`} onClick={() => setDays(30)}>30</button>
                   <button className={`${days == 60 ? 'bg-[#ffc107] text-black' : 'bg-[#000000] hover:bg-[#ffc107] hover:text-black'} py-1 px-1.5 rounded-md font-medium`} onClick={() => setDays(60)}>60</button>
                   <button className={`${days == customDays ? 'bg-[#ffc107] text-black' : 'bg-[#000000] hover:bg-[#ffc107] hover:text-black'} py-1 px-1.5 rounded-md font-medium`} onClick={() => setDays(customDays)}>
-                    <input type="number" id="customDays" placeholder="00" value={customDays} onChange={handleCustomDays} className={`${days == 30 ? 'bg-[#ffc107] text-black' : 'bg-[#000000] hover:bg-[#ffc107] hover:text-black'} py-1 px-1.5 rounded-md font-medium`}/>
+                    <input type="number" id="customDays" placeholder="00" value={customDays} onChange={handleCustomDays} className={`${days == 30 ? 'bg-[#ffc107] text-black' : 'bg-[#000000] hover:bg-[#ffc107] hover:text-black w-3'} py-1 px-1.5 rounded-md font-medium`}/>
                   </button>
                 </div>
                 <div className="relative z-0">
